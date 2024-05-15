6abe1236b61b10bb3ccdb814fc055085b30394c9
lpwanw <phuongtay2000@gmail.com>
2024-05-15 22:41:16 +0700

    fix: bundle audit

--- spec/requests/boards/edit_spec.rb
+++ spec/requests/boards/edit_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe BoardsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:board) { create(:board, user:) }
 
-  describe "GET show" do
+  describe "GET edit" do
     context "when user not signed in" do
       it "redirects to sign in page" do
-        get :show, params: { id: board.id }
+        get :edit, params: { id: board.id }
         expect(response).to redirect_to new_user_session_path
       end
     end
@@ -20,9 +20,9 @@ RSpec.describe BoardsController, type: :controller do
       context "when board is belong to user" do
         let!(:board) { create(:board, user:) }
 
-        before { get :show, params: { id: board.id } }
+        before { get :edit, params: { id: board.id } }
 
-        it { expect(response).to render_template :show }
+        it { expect(response).to render_template :edit }
         it { expect(response).to have_http_status :ok }
         it { expect(assigns(:board)).to eq board }
       end
@@ -31,7 +31,7 @@ RSpec.describe BoardsController, type: :controller do
         let!(:other_user) { create(:user) }
         let!(:other_board) { create(:board, user: other_user) }
 
-        before { get :show, params: { id: other_board.id } }
+        before { get :edit, params: { id: other_board.id } }
 
         it { expect(response).to redirect_to boards_url }
         it { expect(assigns(:board)).to be_nil }
