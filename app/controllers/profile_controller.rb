6c50d1167903a43655c430bd9f28452cc938f954
lpwanw <phuongtay2000@gmail.com>
2024-05-19 13:48:48 +0700

    Add I18n

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -10,9 +10,9 @@ class ProfileController < ApplicationController
   def update
     @profile = current_user.profile || current_user.build_profile
     if @profile.update(profile_params)
-      redirect_to profile_path, notice: I18n.t("Profile updated successfully"), status: :see_other
+      redirect_to profile_path, notice: I18n.t("profile.update.success"), status: :see_other
     else
-      flash[:error] = I18n.t("Failed to update profile")
+      flash[:error] = I18n.t("profile.update.failure")
       render :show, status: :unprocessable_entity
     end
   end
