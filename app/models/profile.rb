9c1ec62904d8444e184f1446ddc1793ee73f042b
lpwanw <phuongtay2000@gmail.com>
2024-05-20 21:32:27 +0700

    Update locale for profile page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,10 +3,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
   validates :name, length: { maximum: 100 }
 
-  validates :dob, presence: true
   validates :dob, comparison: { less_than: Time.current.to_date }
 
   validates :locale, presence: true
