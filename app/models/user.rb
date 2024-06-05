5ca776578e23253faa27c1df973bd7463c28b9ca
lpwanw <phuongtay2000@gmail.com>
2024-06-05 20:04:01 +0700

    Clean Up Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
-  has_many :boards, dependent: :destroy
 
   delegate :name, :dob, :avatar, to: :profile, prefix: true, allow_nil: true
   delegate :locale, to: :profile, prefix: false, allow_nil: true
