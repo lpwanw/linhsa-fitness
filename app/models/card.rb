a3e40cb3b8d984f72e8fe18a529ff2431771c7fd
lpwanw <phuongtay2000@gmail.com>
2024-06-05 20:04:01 +0700

    Clean Up Model

--- app/models/card.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Card < ApplicationRecord
-  belongs_to :list
-
-  validates :title, presence: true
-  validates :title, length: { minimum: 20, maximum: 255 }
-end
--- spec/factories/cards.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :card do
-    title { FFaker::Lorem.sentence }
-    description { FFaker::Lorem.paragraph }
-    list
-  end
-end
