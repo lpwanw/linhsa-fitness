3ca7b116f90c1468f7ec179b70be46321b09bb66
lpwanw <phuongtay2000@gmail.com>
2024-06-09 17:53:16 +0700

    Mockup Home Landing Page

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module SelfHostTaskManagerment
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :vi
 
     I18n.available_locales = [:en, :vi]
   end
