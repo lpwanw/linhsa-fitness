c5e16c4c398cf268a48ac8cf34597da0f4d42182
lpwanw <phuongtay2000@gmail.com>
2024-06-28 23:16:07 +0700

    Dropdown Menu

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+  static targets = ["target"]
+
+  static values = {
+    active: Boolean,
+    enterClass: String,
+    leaveClass: String,
+    type: { type: String, default: "hover"}
+  }
+
+  connect() {
+    console.log(this.enterClassValue)
+    if (this.typeValue === "hover") {
+      this.element.addEventListener("mouseenter", () => {
+        this.activeValue = true;
+        this.targetTarget.classList.remove(...this.leaveClassValue.split(" "))
+        this.targetTarget.classList.add(...this.enterClassValue.split(" "))
+      })
+      this.element.addEventListener("mouseleave", () => {
+        this.activeValue = false;
+        this.targetTarget.classList.remove(...this.enterClassValue.split(" "))
+        this.targetTarget.classList.add(...this.leaveClassValue.split(" "))
+      })
+    }
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-
-  root "home#index"
-  get "up" => "rails/health#show", as: :rails_health_check
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
+  root "marketing#index"
+
+  devise_for :users
+  get "up" => "rails/health#show", as: :rails_health_check
   resource :profile, controller: :profile, only: [:show, :update]
 end
