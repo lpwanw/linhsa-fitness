2c17d1b8af20cafbf03c0820ad42b0925cbf74dd
lpwanw <phuongtay2000@gmail.com>
2024-07-01 19:07:58 +0700

    motionone and header

---
+++ vendor/javascript/@motionone--types.js
@@ -0,0 +1,2 @@
+class MotionValue{setAnimation(i){this.animation=i;i===null||i===void 0?void 0:i.finished.then((()=>this.clearAnimation())).catch((()=>{}))}clearAnimation(){this.animation=this.generator=void 0}}export{MotionValue};
+
