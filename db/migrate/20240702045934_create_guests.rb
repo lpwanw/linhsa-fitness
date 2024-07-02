e6b1e650a722b7939cd36c6c6d7cf901a57e6049
lpwanw <phuongtay2000@gmail.com>
2024-07-02 12:36:13 +0700

    fix: rubocop

--- db/migrate/20240702045934_create_guests.rb
+++ db/migrate/20240702045934_create_guests.rb
@@ -3,7 +3,7 @@
 class CreateGuests < ActiveRecord::Migration[7.1]
   def up
     create_table :guests do |t|
-      t.string :phone, null: false, index: { unique: true }
+      t.string :phone, null: false
       t.string :name, null: false
       t.string :time_to_call
       t.text :note
@@ -11,13 +11,13 @@ class CreateGuests < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
-    execute <<-SQL
+    execute <<-SQL.squish
       CREATE UNIQUE INDEX unique_lower_phone ON guests (lower(phone));
     SQL
   end
 
   def down
-    execute <<-SQL
+    execute <<-SQL.squish
       DROP INDEX IF EXISTS unique_lower_phone;
     SQL
 
