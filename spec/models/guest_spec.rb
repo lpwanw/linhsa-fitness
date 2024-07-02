8530069206629edb34f035960253854e86afdff1
lpwanw <phuongtay2000@gmail.com>
2024-07-02 12:18:48 +0700

    fix: rubocop

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -4,10 +4,10 @@ require "rails_helper"
 
 RSpec.describe Guest, type: :model do
   describe "validations" do
-    subject { build :guest }
+    subject { build(:guest) }
 
     it { is_expected.to validate_presence_of(:phone) }
-    it { is_expected.to validate_uniqueness_of(:phone).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:phone) }
     it { is_expected.to validate_length_of(:phone).is_at_most(10) }
 
     it { is_expected.to validate_presence_of(:name) }
