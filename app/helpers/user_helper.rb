f9d676e5cadd86bc5f2a012612f5564b1e509c64
lpwanw <phuongtay2000@gmail.com>
2024-07-07 20:37:30 +0700

    remove unused

--- app/helpers/user_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module UserHelper
-  def avatar_for(user)
-    user.profile&.persisted? && user.profile_avatar&.attached? ? user.profile_avatar : "default_avatar.png"
-  end
-end
