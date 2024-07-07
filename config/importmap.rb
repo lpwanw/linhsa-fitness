90348403f7499e53897e0c77665c0643fbe81402
lpwanw <phuongtay2000@gmail.com>
2024-07-07 20:30:29 +0700

    Clean up

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,14 +9,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
-pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
-pin "stimulus-use" # @0.52.2
-pin "motion" # @10.18.0
-pin "@motionone/animation", to: "@motionone--animation.js" # @10.18.0
-pin "@motionone/dom", to: "@motionone--dom.js" # @10.18.0
-pin "@motionone/easing", to: "@motionone--easing.js" # @10.18.0
-pin "@motionone/generators", to: "@motionone--generators.js" # @10.18.0
-pin "@motionone/types", to: "@motionone--types.js" # @10.17.1
-pin "@motionone/utils", to: "@motionone--utils.js" # @10.18.0
-pin "hey-listen" # @1.0.8
-pin "tslib" # @2.6.3
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@
 
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  get "up" => "rails/health#show", as: :rails_health_check
 
   root "marketing#index"
-
-  devise_for :users
-  get "up" => "rails/health#show", as: :rails_health_check
-  resource :profile, controller: :profile, only: [:show, :update]
 end
