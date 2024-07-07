f844b2e41b292210611d383653664210eab24863
lpwanw <phuongtay2000@gmail.com>
2024-07-07 21:55:29 +0700

    double quote

--- app/javascript/controllers/dark_controller.js
+++ app/javascript/controllers/dark_controller.js
@@ -6,33 +6,33 @@ export default class extends Controller {
 
   connect() {
     console.log(this.darkIconTarget.classList)
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      this.lightIconTarget.classList.remove('hidden');
+    if (localStorage.getItem("color-theme") === "dark" || (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+      this.lightIconTarget.classList.remove("hidden");
     } else {
-      this.darkIconTarget.classList.remove('hidden');
+      this.darkIconTarget.classList.remove("hidden");
     }
   }
 
   toggle() {
-    this.darkIconTarget.classList.toggle('hidden');
-    this.lightIconTarget.classList.toggle('hidden');
-    if (localStorage.getItem('color-theme')) {
-      if (localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark', 'bg-gray-950');
-        localStorage.setItem('color-theme', 'dark');
+    this.darkIconTarget.classList.toggle("hidden");
+    this.lightIconTarget.classList.toggle("hidden");
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark", "bg-gray-950");
+        localStorage.setItem("color-theme", "dark");
       } else {
-        document.documentElement.classList.remove('dark', 'bg-gray-950');
-        localStorage.setItem('color-theme', 'light');
+        document.documentElement.classList.remove("dark", "bg-gray-950");
+        localStorage.setItem("color-theme", "light");
       }
 
       // if NOT set via local storage previously
     } else {
-      if (document.documentElement.classList.contains('dark')) {
-        document.documentElement.classList.remove('dark', 'bg-gray-950');
-        localStorage.setItem('color-theme', 'light');
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark", "bg-gray-950");
+        localStorage.setItem("color-theme", "light");
       } else {
-        document.documentElement.classList.add('dark', 'bg-gray-950');
-        localStorage.setItem('color-theme', 'dark');
+        document.documentElement.classList.add("dark", "bg-gray-950");
+        localStorage.setItem("color-theme", "dark");
       }
     }
   }
