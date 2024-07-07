86a4a4e0f27025e546c3d13b910ed9f02858d61c
lpwanw <phuongtay2000@gmail.com>
2024-07-07 21:54:55 +0700

    Dark toggle button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
-
-  private
-
-  def switch_locale(&)
-    locale = current_user&.locale || I18n.default_locale
-    I18n.with_locale(locale, &)
-  end
 end
