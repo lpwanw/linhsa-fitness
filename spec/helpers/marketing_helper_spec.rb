4286632a73ea6a5e6c66ac275ed05be914d6a675
lpwanw <phuongtay2000@gmail.com>
2024-07-08 18:15:12 +0700

    rspec helper

---
+++ spec/helpers/marketing_helper_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe MarketingHelper, type: :helper do
+  describe '#marketing_navigations' do
+    subject { helper.marketing_navigations }
+
+    it { is_expected.to be_an(Array) }
+    it { is_expected.to all(be_a(Hash)) }
+    it do
+      is_expected.to(
+        eq(
+          [
+            {
+              link: root_path,
+              name: :clubs,
+            }, {
+              link: root_path,
+              name: :calendars,
+            }, {
+              link: root_path,
+              name: :courses,
+            }, {
+              link: root_path,
+              name: :prices,
+            }, {
+              link: root_path,
+              name: :about,
+            },
+          ]
+        )
+      )
+    end
+  end
+end
