0f5309208216b0010a5f19785e754b58ecc0d4a4
lpwanw <phuongtay2000@gmail.com>
2024-07-09 14:36:12 +0700

    Footer

---
+++ app/components/marketing/footer.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Marketing::Footer < ViewComponent::Base
+
+end
---
+++ spec/components/marketing/footer_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Marketing::Footer, type: :component do
+  subject { render_inline described_class.new }
+
+  it { is_expected.to have_link(href: "#")}
+end
