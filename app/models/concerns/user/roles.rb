f86f381247349508ee93f763b4ca530f3b8ec9de
lpwanw <phuongtay2000@gmail.com>
2024-07-09 17:15:50 +0700

    Role Setup

---
+++ app/models/concerns/user/roles.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module User::Roles
+  extend ActiveSupport::Concern
+
+  included do
+    rolify
+
+    after_create :assign_default_role
+  end
+
+  def assign_default_role
+    add_role(UserRoles.member) if roles.blank?
+  end
+
+  UserRoles.roles.each do |role|
+    define_method("#{role}?") do
+      has_role? role
+    end
+  end
+end
---
+++ spec/models/user_roles_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserRoles do
+
+  UserRoles.roles.each do |role|
+    describe ".#{role}" do
+      subject { described_class.public_send(role) }
+
+      it { is_expected.to eq role }
+    end
+  end
+
+  describe ".roles" do
+    subject { described_class.roles }
+
+    it { is_expected.to eq %i[member teacher admin]}
+  end
+end
