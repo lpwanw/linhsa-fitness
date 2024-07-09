6758fef93110305d78fb7d1624e873ec96cf40c6
lpwanw <phuongtay2000@gmail.com>
2024-07-09 15:56:12 +0700

    fix active record doctor

--- .active_record_doctor.rb
+++ .active_record_doctor.rb
@@ -2,29 +2,15 @@
 
 ActiveRecordDoctor.configure do
   # Global settings affect all detectors.
-  global :ignore_tables, [
-    # Ignore internal Rails-related tables.
-    "ar_internal_metadata",
-    "schema_migrations",
-    "active_storage_blobs",
-    "active_storage_attachments",
-    "action_text_rich_texts",
-  ]
+  global :ignore_tables, %w[ar_internal_metadata schema_migrations active_storage_blobs active_storage_attachments action_text_rich_texts]
 
-  global :ignore_models, [
-    "ActionText::RichText",
-    "ActionText::EncryptedRichText",
-    "ActiveStorage::VariantRecord",
-    "ActiveStorage::Blob",
-    "ActiveStorage::Attachment",
-    "ActionMailbox::InboundEmail",
-  ]
+  global :ignore_models, %w[ActionText::RichText ActionText::EncryptedRichText ActiveStorage::VariantRecord ActiveStorage::Blob ActiveStorage::Attachment ActionMailbox::InboundEmail]
 
   detector :short_primary_key_type,
            enabled: false
 
   detector :missing_presence_validation,
-           ignore_attributes: ["User.encrypted_password"]
+           ignore_attributes: %w[User.encrypted_password User.sign_in_count]
 
   detector :mismatched_foreign_key_type,
            ignore_columns: ["active_storage_variant_records.blob_id"]
