9be842c98d1145aa82a4dbe3efd8d14a84a9e157
lpwanw <phuongtay2000@gmail.com>
2024-07-09 14:37:02 +0700

    fix: rubocop

--- app/components/marketing/footer.rb
+++ app/components/marketing/footer.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Marketing::Footer < ViewComponent::Base
-
 end
--- spec/components/marketing/footer_spec.rb
+++ spec/components/marketing/footer_spec.rb
@@ -5,5 +5,5 @@ require "rails_helper"
 RSpec.describe Marketing::Footer, type: :component do
   subject { render_inline described_class.new }
 
-  it { is_expected.to have_link(href: "#")}
+  it { is_expected.to have_link(href: "#") }
 end
