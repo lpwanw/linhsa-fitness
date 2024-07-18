cee24c537105e7ffc6cc8aced38eb64714bc5553
lpwanw <phuongtay2000@gmail.com>
2024-07-18 17:21:22 +0700

    Create Guest

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -4,9 +4,21 @@ class GuestsController < ApplicationController
   before_action :authenticate_turbo_frame_request!, only: %i[new]
   def new; end
 
-  def create; end
+  def create
+    @guest = Guest.new(guest_params)
+
+    if @guest.save
+      flash.now[:notice] = t(".success")
+    else
+      flash.now[:alert] = t(".failed")
+    end
+  end
 
   private
+
+  def guest_params
+    params.required(:guest).permit(:name, :phone, :note, :free_time)
+  end
 
   def authenticate_turbo_frame_request!
     return if turbo_frame_request?
