7b4a15ec67b8e4ee37b5ec209a4b924551e0ca1d
lpwanw <phuongtay2000@gmail.com>
2024-07-20 13:16:19 +0700

    fix: remove unused method

--- app/helpers/role_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RoleHelper
-  def role_root_path(role = nil)
-    role = current_role if UserRoles.roles.exclude?(role&.to_sym)
-
-    public_send("#{role}_root_path")
-  end
-end
