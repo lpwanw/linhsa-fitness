705fd5b0216ca3a41b99d4a6b093a9838dc61b71
lpwanw <phuongtay2000@gmail.com>
2024-07-20 11:45:16 +0700

    Remove User reduant rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@
 class User < ApplicationRecord
   include User::Roles
 
-  rolify
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
