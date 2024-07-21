5c8897490580906aeb947d603a4c9d0faa0cbff9
lpwanw <phuongtay2000@gmail.com>
2024-07-21 19:56:04 +0700

    action_cable

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  mount ActionCable.server, at: '/cable'
+  mount ActionCable.server, at: "/cable"
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "marketing#index"
