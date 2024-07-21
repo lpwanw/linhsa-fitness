25cd0c58c4f95c4fecf724d343d564b8fcd4e9a3
lpwanw <phuongtay2000@gmail.com>
2024-07-21 22:00:15 +0700

    use Yugabyte

--- db/migrate/20240709090242_rolify_create_roles.rb
+++ db/migrate/20240709090242_rolify_create_roles.rb
@@ -2,7 +2,7 @@
 
 class RolifyCreateRoles < ActiveRecord::Migration[7.1]
   def change
-    create_table(:roles) do |t|
+    create_table(:roles, id: :uuid) do |t|
       t.string :name
       t.references :resource, polymorphic: true
 
--- db/migrate/20240721035706_create_index_on_name_and_phone_of_guest.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class CreateIndexOnNameAndPhoneOfGuest < ActiveRecord::Migration[7.1]
-  def change
-    add_index :guests, :name
-    add_index :guests, :phone
-  end
-end
