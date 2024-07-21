e77537ec898ffd4a0882676d8195e78a580bc270
lpwanw <phuongtay2000@gmail.com>
2024-07-21 18:51:14 +0700

    cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = ENV.fetch "WEBSOCKET_URL"
+  config.action_cable.allowed_request_origins = [ ENV.fetch("HOST_URL") ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
