309ba24a8c6ffa50b0ba82837e23e858099d7724
lpwanw <phuongtay2000@gmail.com>
2024-07-22 21:06:49 +0700

    Fix Mising ENV

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Cloudinary.config_from_url(ENV.fetch("CLOUDINARY_URL")) if Rails.env.production?
+Cloudinary.config_from_url(ENV.fetch("CLOUDINARY_URL", nil)) if Rails.env.production?
