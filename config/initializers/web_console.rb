5edd16d605ec1919a2a762065d9a6c15059b6cc6
lpwanw <phuongtay2000@gmail.com>
2024-07-23 19:41:32 +0700

    Allow Console

--- config/initializers/web_console.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-if Rails.env.production?
-  Rails.application.configure do
-    config.web_console.permissions = ENV.fetch("CONSOLE_ALLOW_IPS", "").split
-  end
-end
