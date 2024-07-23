c38150901ae73f729ab9beec6344daaedec2352c
lpwanw <phuongtay2000@gmail.com>
2024-07-23 16:32:33 +0700

    Allow Console

--- app/controllers/admin/console_controller.rb
+++ app/controllers/admin/console_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::ConsoleController < Admin::BaseController
+  config.web_console.permissions = '0.0.0.0/0'
+
   layout "devise"
   http_basic_authenticate_with name: ENV.fetch("CONSOLE_USER"), password: ENV.fetch("CONSOLE_PASSWORD"), only: :show
 
