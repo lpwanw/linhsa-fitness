671c758c06321098cc32b9ee03f6a08724a25ce3
lpwanw <phuongtay2000@gmail.com>
2024-07-23 15:03:26 +0700

    Job to save self

---
+++ config/initializers/good_job.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  config.good_job.enable_cron = true
+  config.good_job.cron = {
+    save_my_self: { # each recurring job must have a unique key
+      cron: "*/1 * * * *", # cron-style scheduling format by fugit gem
+      class: "KeepMeAliveJob", # name of the job class as a String; must reference an Active Job job class
+      description: "Keep the service running on Render", # optional description that appears in Dashboard
+    },
+  }
+end
